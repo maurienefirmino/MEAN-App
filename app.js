6d537249d82606ae13973dd024ae6b62c86e6f94
maurienefirmino <maurienefirmino@gmail.com>
2016-10-20 21:41:12 -0300

    Editar, novo corrigido

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ http.createServer((req,res) => {
 	res.writeHead(200,{"Content-Type":"text/html"});
 	res.end(index);
 }).listen(80,() => {
-	console.log('Servidor Aberto em localhost');
+	console.log('Servidor Web Aberto em localhost');
 });
--- js/app.js
+++ js/app.js
@@ -9,40 +9,70 @@ angular.module('appPessoas', ['ngRoute']).config(['$routeProvider', function ($r
 		templateUrl:'pessoas.html',
 		controller:'PessoasController',
 		controllerAs:'P'
+	}).when('/pessoas/editar/:id',{
+		templateUrl:'edicao.html',
+		controller:'PessoasController',
+		controllerAs:'P'
+	}).when('/pessoas/detalhes/:id',{
+		templateUrl:'pagina-pessoa.html',
+		controller:'PessoasController',
+		controllerAs:'P'
 	}).otherwise({ redirectTo: '/pessoas' });
 	
 }]).controller('PessoasController',PessoasController);
 
-function PessoasController($http){
+function PessoasController($http, $routeParams){
 	var vm = this;
 	vm.dados = [];
-
+	vm.routeParams = $routeParams;
 	$http({
 		method: 'GET',
 		url: 'http://localhost:3000/pessoas'
 	}).then(function successCallback(res) {
 		vm.dados = res.data;
-		console.log(vm.dados);
 	}, function errorCallback(res) {
 		console.log('Error');
 	});
 
 	vm.salva = function salva(form){
+		console.log(form);
 		$http({
 			method: 'post',
 			data: form,
 			url: 'http://localhost:3000/pessoas',
 
 		}).then(function successCallback(res) {
-			alert('salvo!');
+			alert('Salvo!');
 
 		}, function errorCallback(res) {
 			alert('ERRO!');
 		});
 	};
 
-	vm.deleta = function deleta($index){
+	vm.edita = function edita($index){
 
+	}
+
+	vm.deleta = function deleta(id){
+
+		var msg = confirm("Tem certeza que deseja excluir este registro?");
+
+		if (msg == true) {
+
+			$http({
+				method: 'GET',
+				url: 'http://localhost:3000/pessoas/delete/'+id
+			}).then(function successCallback(res) {
+
+			}, function errorCallback(res) {
+				console.log('Error');
+			});
+
+			console.log(vm.dados);
+
+			vm.dados = vm.dados.filter(function(el){return el._id !== id});
+
+		}
 	}
 
 }
