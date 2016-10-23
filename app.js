4843603e01d400f59f77e78884fd9499ab609073
maurienefirmino <maurienefirmino@gmail.com>
2016-10-23 08:05:16 -0300

    alterações finais

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const index = fs.readFileSync("index.html");
 http.createServer((req,res) => { 
 	res.writeHead(200,{"Content-Type":"text/html"});
 	res.end(index);
-}).listen(80,() => {
+}).listen(5000,() => {
 	console.log('Servidor Web Aberto em localhost');
 });
